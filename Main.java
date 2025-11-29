5492afba156cfa2261b81d9ee9a23753b810c836
ItzWaFFeX <164030036+ItzWaFFeX@users.noreply.github.com>
2025-11-29 18:55:10 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+interface MyInterface{
+    public void method1();
+    public void method2();
+}
+
+class Main implements MyInterface{
+    public void method1(){
+        System.err.println("Implementation of method 1");
+    }
+
+    public void method2(){
+        System.err.println("Implementation of method 2");
+    }
+
+    public static void main(String[] args) {
+    MyInterface obj = new Main();
+    obj.method1();
+    obj.method2();
+}
+
+}
+
